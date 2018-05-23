512dfb4ee32c318c75c81f900a5558a204d51bfa
bkrigel <benjamin.krigel@gmail.com>
2018-05-23 12:20:58 -0400

    A couple of toughies

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,68 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    @@all << song
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      if song.name == name
+        song
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if find_by_name(name)
+      find_by_name(name)
+    else
+      create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    split_filename_at_filetype = filename.split(".")
+    split_filename = split_filename_at_filetype[0]
+    split_filename_at_name = split_filename.split(" - ")
+    artist_name = split_filename_at_name[0]
+    name = split_filename_at_name[1]
+    song = Song.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
